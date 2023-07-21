dffe180d38588d278eefe9bbf7d263b9e8f5596c
matjazkic <matjaz.kic@gmail.com>
2023-07-21 10:32:44 +0200

    added working cart with functional buttons, last part of practice project unitl API

--- src/Components/Store/CartProvider.js
+++ src/Components/Store/CartProvider.js
@@ -34,6 +34,27 @@ const cartReducer = (state, action) => {
       totalAmount: updateTotalAmount,
     };
   }
+  if (action.type === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updateTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updateTotalAmount,
+    };
+  }
+  return defaultCartState;
 };
 
 const CartProvider = (props) => {
